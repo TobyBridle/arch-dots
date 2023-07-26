08c2e92372a99f0c8b0e1e1ef1448314c25f07f3
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-07-26 21:56:20 +0100

    wip(colorscheme): adaptive waybar colors
    Adapative Waybar colouring using pywal
    Currently, it doesn't look too great. I
    need to figure out which b16 colours work well together
    to be able to make a good looking standard for the scheme.

--- scripts/set_swww_wallpaper.sh
+++ scripts/set_swww_wallpaper.sh
@@ -14,3 +14,15 @@ if [[ -z "$SELECTION" ]]; then
 fi
 notify-send "Changing Wallpaper" "Changing Wallpaper to $WALLPAPER_DIR$SELECTION"
 swww img "$WALLPAPER_DIR$SELECTION" -t center --transition-fps 165
+{{ #if (eq_string use-native-colorscheme "false") }}
+wal --cols16 -s -t -i $WALLPAPER_DIR$SELECTION
+{{ #if dotter.packages.waybar }}
+WAL_WAYBAR_COLORS=$(cat ~/.cache/wal/colors-waybar.css)
+WAYBAR_CFG="$(cat ~/.config/waybar/style.css | sed -n "1,/@define-color \(shadow\|color15\)/d; /./p")"
+NEW_CFG=$(printf "%s\n%s" "${WAL_WAYBAR_COLORS}" "${WAYBAR_CFG}")
+printf "%s" "$NEW_CFG" >~/.config/waybar/style.css
+killall waybar
+waybar &
+disown
+{{ /if }}
+{{ /if }}
