8c2af5d564d3f5c8059369d60fbcf9e5bc47b556
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-07-26 21:42:33 +0100

    fix: wezterm antive thingy

--- wezterm/theme.lua
+++ wezterm/theme.lua
@@ -9,7 +9,7 @@ return {
 }
 {{ /if }}
 
-{{ #if (eq_string use-wezterm-colorscheme "false") }}
+{{ #if (eq_string use-native-colorscheme "false") }}
 local colors, _ = require("wezterm").color.load_base16_scheme(os.getenv( "HOME" ) .. "/.config/wezterm/colors/dynamic-base16.yaml") 
 return {
     colors = colors
