fe6c997c74b81c3961406bc54417949e77526400
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-07-31 16:36:19 +0100

    fix(colorscheme): explicit TOMLQ path

--- scripts/colorscheme_switcher.sh
+++ scripts/colorscheme_switcher.sh
@@ -1,12 +1,13 @@
 #!/bin/env sh
 
 DOTTER_CONF="{{dotter.current_dir}}/.dotter/global.toml"
+TOMLQ="{{dotter.current_dir}}/bin/tomlq"
 # If you are that stupid that you have a "" in your colorschemes, uncomment the below
 #THEME=$(/bin/cat $DOTTER_CONF | tomlq -c .global.variables.theming | sed -E 's/\{"colorschemes":\W?\[(".+"\W?|".+",\W?)*\]\}/\1/' | tr ',' '\n' | cut -d '"' -f2 | grep "\S" | rofi -dmenu)
 # THEME=$(/bin/cat $DOTTER_CONF | tomlq -c .global.variables.theming | sed -E 's/\{"colorschemes":\W?\[(".+"\W?|".+",\W?)*\]\}/\1/' | tr ',' '\n' | cut -d '"' -f2 | rofi -dmenu -theme "~/.config/rofi/styles/theme-selector.rasi")
 BASE=$(echo $HOME | sed "s/\//\\//g")
-THEME=$(/bin/cat $DOTTER_CONF | tomlq -c .global.variables.theming | sed -E 's/\{"colorschemes":\s?\[(".+"\s?|".+",\s?)*\]\}/\1/' | tr ',' '\n' | while read -r theme; do
-    fp=$(/bin/cat "$DOTTER_CONF" | tomlq ".$(echo "$theme" | tr -d '\"').variables.default_wallpaper" | tr -d '"' | sed -E "s;^~/;$BASE/;")
+THEME=$(/bin/cat $DOTTER_CONF | $TOMLQ -c .global.variables.theming | sed -E 's/\{"colorschemes":\s?\[(".+"\s?|".+",\s?)*\]\}/\1/' | tr ',' '\n' | while read -r theme; do
+    fp=$(/bin/cat "$DOTTER_CONF" | $TOMLQ ".$(echo "$theme" | tr -d '\"').variables.default_wallpaper" | tr -d '"' | sed -E "s;^~/;$BASE/;")
     stripped=$(basename $fp)
     if [[ ! -f /tmp/${stripped%.*}.jpg ]]; then
         convert $fp -thumbnail 500x500^ -gravity center -extent 500x500 /tmp/${stripped%.*}.jpg
@@ -20,10 +21,10 @@ if [[ -z "$THEME" ]]; then
 fi
 
 /bin/cat <<EOM >$DOTTER_CONF
-$(/bin/cat $DOTTER_CONF | tomlq ".colorscheme.depends=[\"$THEME\"]" | yj -jt)
+$(/bin/cat $DOTTER_CONF | $TOMLQ ".colorscheme.depends=[\"$THEME\"]" | yj -jt)
 EOM
 
-WALLPAPER=$(/bin/cat $DOTTER_CONF | tomlq ."$THEME".variables | sed -nE 's/"default_wallpaper":\s*"(.+)"(,?)/\1/p')
+WALLPAPER=$(/bin/cat $DOTTER_CONF | $TOMLQ ."$THEME".variables | sed -nE 's/"default_wallpaper":\s*"(.+)"(,?)/\1/p')
 WALLPAPER_FILE=$(echo $WALLPAPER | sed -E "s;~;$BASE;")
 swww img "$WALLPAPER_FILE" -t center --transition-fps 165
 killall {{ noti_manager }}
